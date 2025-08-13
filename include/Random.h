b97278e6b329a9c4593275c2a0b1e13ded0876d7
Darthsae <sockmit2007@gmail.com>
2025-08-13 03:43:00 -0400

    Mew

--- include/Random.h
+++ include/Random.h
@@ -2,9 +2,17 @@
 #define RANDOM_H
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /// @brief Generate a random @c uint32_t between @p a_min and @p a_max.
 /// @param[in] a_min The minimum size of the number.
 /// @param[in] a_max The maximum size of the number.
 /// @return A random @c uint32_t between @p a_min and @p a_max.
 uint32_t RandomUniformUInt32(uint32_t, uint32_t);
+
+#ifdef __cplusplus
+}
+#endif
 #endif
--- src/Random.c
+++ src/Random.c
@@ -1,9 +1,17 @@
 #include <random.h>
 #include <stdlib.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 uint32_t RandomUniformUInt32(uint32_t a_min, uint32_t a_max) {
     uint32_t r = rand();
     const uint32_t range = a_max - a_min;
     while ((r = rand()) >= range);
     return a_min + r;
 }
+
+#ifdef __cplusplus
+}
+#endif
